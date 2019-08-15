4654511e9165ab62f98f2e7fab90a699d14bc1a9
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2019-08-14 23:20:19 -0500

    Adds test case to validate median

--- src/test/java/com/hedera/services/exchange/ERTprocTestCases.java
+++ src/test/java/com/hedera/services/exchange/ERTprocTestCases.java
@@ -1,7 +1,21 @@
 package com.hedera.services.exchange;
 
 import com.hedera.services.exchange.ERTproc;
+import com.hedera.services.exchange.exchanges.Bitrex;
+import com.hedera.services.exchange.exchanges.Coinbase;
+import com.hedera.services.exchange.exchanges.Liquid;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ERTprocTestCases {
 
@@ -9,7 +23,45 @@ public class ERTprocTestCases {
             0.0, 0.0, "0");
 
     @Test
-    public void testMedian(){
-        ertProcess.call();
+    public void testMedian() throws IOException {
+        this.setExchanges();
+        final double median = ertProcess.call();
+        assertEquals(0.00954162, median);
+    }
+
+    private void setExchanges() throws IOException {
+        final String bitrexResult = "{\"success\":true,\"message\":\"Data Sent\",\"result\":{\"Bid\":0.00952751,\"Ask\":0.00753996," +
+                "\"Last\":0.00954162}}";
+        final InputStream bitrexJson = new ByteArrayInputStream(bitrexResult.getBytes());
+        final HttpURLConnection bitrexConnection = mock(HttpURLConnection.class);
+        when(bitrexConnection.getInputStream()).thenReturn(bitrexJson);
+        new MockUp<Bitrex>() {
+            @Mock
+            HttpURLConnection getConnection() {
+                return bitrexConnection;
+            }
+        };
+
+        final String coinbaseResult = "{\"data\":{\"currency\":\"USD\", \"rates\":{\"HBAR\":\"0.0098\"}}}";
+        final InputStream coinbaseJson = new ByteArrayInputStream(coinbaseResult.getBytes());
+        final HttpURLConnection coinbaseConnection = mock(HttpURLConnection.class);
+        when(coinbaseConnection.getInputStream()).thenReturn(coinbaseJson);
+        new MockUp<Coinbase>() {
+            @Mock
+            HttpURLConnection getConnection() {
+                return coinbaseConnection;
+            }
+        };
+
+        final String liquidResult = "{\"product_type\":\"CurrencyPair\", \"code\":\"CASH\", \"exchange_rate\": 0.0093}";
+        final InputStream liquidJson = new ByteArrayInputStream(liquidResult.getBytes());
+        final HttpURLConnection liquidConnection = mock(HttpURLConnection.class);
+        when(liquidConnection.getInputStream()).thenReturn(liquidJson);
+        new MockUp<Liquid>() {
+            @Mock
+            HttpURLConnection getConnection() {
+                return liquidConnection;
+            }
+        };
     }
 }
