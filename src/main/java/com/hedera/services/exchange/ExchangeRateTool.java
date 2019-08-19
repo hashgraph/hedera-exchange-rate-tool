bd978b6bb820721f3fb0ae14d45d19ef5e51dcea
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2019-08-19 16:14:47 -0500

    00011: Adds method to update and validate Exchange rates

--- src/main/java/com/hedera/services/exchange/ExchangeRateTool.java
+++ src/main/java/com/hedera/services/exchange/ExchangeRateTool.java
@@ -4,11 +4,15 @@ import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.HederaException;
 import com.hedera.hashgraph.sdk.TransactionId;
 import com.hedera.hashgraph.sdk.account.AccountId;
-import com.hedera.hashgraph.sdk.crypto.Key;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import com.hedera.hashgraph.sdk.file.FileContentsQuery;
 import com.hedera.hashgraph.sdk.file.FileId;
 import com.hedera.hashgraph.sdk.file.FileUpdateTransaction;
+import com.hedera.hashgraph.sdk.proto.FileGetContentsQuery;
+import com.hedera.hashgraph.sdk.proto.FileGetContentsResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bouncycastle.util.Arrays;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -18,8 +22,10 @@ import java.util.Map;
 
 public class ExchangeRateTool {
 
+    private static final String OPERATOR_KEY = "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137";
+
     // logger object to write logs into
-    private static final Logger log = LogManager.getLogger(ExchangeRateTool.class);
+    private static final Logger LOGGER = LogManager.getLogger(ExchangeRateTool.class);
 
     // member variables
     private String privateKey;
@@ -54,21 +60,34 @@ public class ExchangeRateTool {
                 0l,
                 "0");
         final ExchangeRate exchangeRate = proc.call();
+        final byte[] exchangeRateAsBytes = exchangeRate.toExchangeRateSet().toByteArray();
+        final AccountId operatorId = AccountId.fromString("0.0.57");
 
-        final AccountId accountId = new AccountId(3);
+        final AccountId nodeId = new AccountId(3);
         final String address = "0.testnet.hedera.com:50211";
         final Map<AccountId, String> nodes = new HashMap<>();
-        nodes.put(accountId, address);
+        final FileId fileId = FileId.fromString("0.0.112");
+        nodes.put(nodeId, address);
 
-        final Key operatorKey = null;
-        final Client client = new Client(nodes).setMaxTransactionFee(100_000_000_000L);
-        final TransactionId transaction = new FileUpdateTransaction(client)
-                .setFileId(FileId.fromString("0.0.112"))
-                .setContents(exchangeRate.toExchangeRateSet().toByteArray())
-                .addKey(operatorKey)
-                .execute();
+        final Ed25519PrivateKey operatorKey =  Ed25519PrivateKey.fromString(OPERATOR_KEY);;
+        final Client client = new Client(nodes)
+                .setMaxTransactionFee(1_000_000_000L)
+                .setOperator(operatorId, operatorKey);
+        final FileUpdateTransaction fileUpdateTransaction = new FileUpdateTransaction(client)
+                .setFileId(fileId)
+                .setContents(exchangeRateAsBytes)
+                .addKey(operatorKey.getPublicKey());
 
-        final Instant validStart = transaction.getValidStart();
+        final TransactionId transactionId = fileUpdateTransaction.execute();
+        final Instant validStart = transactionId.getValidStart();
+
+
+        final FileGetContentsResponse contentsResponse = new FileContentsQuery(client).setFileId(fileId).execute();
+
+        final byte[] contentsRetrieved = contentsResponse.getFileContents().getContents().toByteArray();
+        if (Arrays.areEqual(exchangeRateAsBytes, contentsRetrieved)) {
+            throw new RuntimeException("The ExchangeRates weren't updated successfully");
+        }
     }
 
     public String getPrivateKey() {
