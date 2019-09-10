b85a6b47896f2f7ed02fc37093bc7f5449b28991
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2019-09-10 12:25:27 -0500

    00040: Removes uncommented code

--- deploy_new_env.sh
+++ deploy_new_env.sh
@@ -108,38 +108,38 @@ DATABASE_NAME="$DATABASE_NAME$NAME"
 
 echo "Creating database instance ${DATABASE_NAME}"
 
-#aws rds create-db-instance \
-#    --allocated-storage 100 \
-#    --max-allocated-storage 500 \
-#    --db-instance-class db.m5.xlarge \
-#    --db-instance-identifier "$DATABASE_NAME" \
-#    --engine postgres \
-#    --enable-cloudwatch-logs-exports '["postgresql","upgrade"]' \
-#    --master-username "$USERNAME" \
-#    --master-user-password "$PASSWORD" \
-#    --db-name exchangeRate \
-#    --port 5432 \
-#    --engine-version 11.4 \
-#    --storage-type gp2 \
-#    --copy-tags-to-snapshot \
-#    --enable-iam-database-authentication \
-#    --enable-performance-insights \
-#    --publicly-accessible \
-#    --region "$REGION"
-#
-#echo "Waiting for database ${DATABASE_NAME} to become available"
-#
-#aws rds wait db-instance-available \
-#    --db-instance-identifier "${DATABASE_NAME}"  \
-#    --region "$REGION"
-#
-#echo "Retrieving endpoint for database ${DATABASE_NAME}"
-#
-#DATABASE_ENDPOINT=$(aws rds describe-db-instances  \
-#                        --db-instance-identifier "$DATABASE_NAME" \
-#                        --region "$REGION" \
-#                        --query 'DBInstances[0].Endpoint.Address' \
-#                        --output text)
+aws rds create-db-instance \
+    --allocated-storage 100 \
+    --max-allocated-storage 500 \
+    --db-instance-class db.m5.xlarge \
+    --db-instance-identifier "$DATABASE_NAME" \
+    --engine postgres \
+    --enable-cloudwatch-logs-exports '["postgresql","upgrade"]' \
+    --master-username "$USERNAME" \
+    --master-user-password "$PASSWORD" \
+    --db-name exchangeRate \
+    --port 5432 \
+    --engine-version 11.4 \
+    --storage-type gp2 \
+    --copy-tags-to-snapshot \
+    --enable-iam-database-authentication \
+    --enable-performance-insights \
+    --publicly-accessible \
+    --region "$REGION"
+
+echo "Waiting for database ${DATABASE_NAME} to become available"
+
+aws rds wait db-instance-available \
+    --db-instance-identifier "${DATABASE_NAME}"  \
+    --region "$REGION"
+
+echo "Retrieving endpoint for database ${DATABASE_NAME}"
+
+DATABASE_ENDPOINT=$(aws rds describe-db-instances  \
+                        --db-instance-identifier "$DATABASE_NAME" \
+                        --region "$REGION" \
+                        --query 'DBInstances[0].Endpoint.Address' \
+                        --output text)
 
 echo "${DATABASE_NAME} has endpoint ${DATABASE_ENDPOINT}"
 
