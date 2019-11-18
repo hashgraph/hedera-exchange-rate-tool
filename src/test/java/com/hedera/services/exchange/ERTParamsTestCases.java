6172bc21d16dd3e7a413da2a7b4976dada45699f
Anirudh Ghanta <anirudh.ghanta@hedera.com>
2019-11-18 16:22:31 -0600

    updated default config files

--- src/test/java/com/hedera/services/exchange/ERTParamsTestCases.java
+++ src/test/java/com/hedera/services/exchange/ERTParamsTestCases.java
@@ -9,7 +9,7 @@ public class ERTParamsTestCases {
     @Test
     public void readCofiguration() throws Exception {
         final ERTParams ertParams = ERTParams.readConfig("src/test/resources/configs/config.json");
-        assertEquals(20, ertParams.getBound());
+        assertEquals(25, ertParams.getBound());
         assertEquals("0.0.57", ertParams.getPayAccount());
         assertEquals("0.0.112", ertParams.getFileId());
         assertEquals(15_000, ertParams.getValidationDelayInMilliseconds());
@@ -18,7 +18,7 @@ public class ERTParamsTestCases {
     @Test
     public void readCofigurationFail() throws Exception {
         final ERTParams ertParams = ERTParams.readConfig("src/test/resources/configs/config1.json");
-        assertEquals(20, ertParams.getBound());
+        assertEquals(25, ertParams.getBound());
         assertEquals("0.0.57", ertParams.getPayAccount());
         assertEquals(null, ertParams.getFileId());
         assertEquals(15_000, ertParams.getValidationDelayInMilliseconds());
