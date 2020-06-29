0aaf6c3993f20a1f49a52dd8b6b0e11d4d2b6c9e
Cesar Agustin Garcia Vazquez <cesarvazquez@swirlds.com>
2020-06-29 16:56:14 -0500

    0092: Adds lambda function name

--- src/test/java/com/hedera/exchange/ExchangeRateUtilsTestCases.java
+++ src/test/java/com/hedera/exchange/ExchangeRateUtilsTestCases.java
@@ -11,7 +11,7 @@ public class ExchangeRateUtilsTestCases {
 		final String expectedValue = "https://s3.amazonaws.com/exchange.rate.config.integration/config.json";
 		final String encryptedValue = "AQICAHi3BYYdRzjj1ZR5ij/3mN6+GWqEbw7NTAG0fm7nzYo3MwHyBlKsmA+1lepLUe" +
 				"+0rgeFAAAApzCBpAYJKoZIhvcNAQcGoIGWMIGTAgEAMIGNBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDLHJjkIANloMVIhCdgIBEIBg9HnXBKnxE3c4H5/17ilQR0G6DqZKH6dzBnhkUAjYbg1sBuStjVA8rQwBUtiSKO7b5ehQh+OxnrJxVbHAZNylSH71fr7OICMI3iA2qkIM8gtWNG1htphGhkDLCRcaw5Xh";
-
-		assertEquals(expectedValue, ExchangeRateUtils.getDecryptedValueFromAWS(encryptedValue));
+		final String lambdaFunctioName = "exchange-rate-tool-lambda-integration";
+		assertEquals(expectedValue, ExchangeRateUtils.getDecryptedValueFromAWS(encryptedValue, lambdaFunctioName));
 	}
 }
