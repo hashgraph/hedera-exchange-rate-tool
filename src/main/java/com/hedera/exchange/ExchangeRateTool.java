9a918dc43d35adeab9cc3cab2333dda33a641a3b
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2025-01-29 11:23:38 -0600

    Fix NPE when there is a SNS failure (#214)
    * fix npe
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
    
    * Update pom
    
    * fix exception and use old addressbook when new addressbook can't be fetched
    
    ---------
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- src/main/java/com/hedera/exchange/ExchangeRateTool.java
+++ src/main/java/com/hedera/exchange/ExchangeRateTool.java
@@ -9,7 +9,7 @@ package com.hedera.exchange;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *
@@ -223,7 +223,8 @@ public class ExchangeRateTool {
                             exchangeRate,
                             midnightExchangeRate,
                             hederaClient,
-                            ertParams
+                            ertParams,
+                            ertAddressBookFromPreviousRun
                     );
 
                     if(!newAddressBook.getNodes().isEmpty()) {
